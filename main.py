6744903530646bc0c81cb353e9feac807e10c80b
Mónica Briceño <monibrice140@gmail.com>
2026-01-15 19:11:49 +0100

    Primer commit del proyecto Embajada

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, EmailStr, field_validator, ValidationError
+from typing import Optional, List
+import re
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"ping": "pong"}
